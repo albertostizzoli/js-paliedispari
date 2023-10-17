158b0d1e80c3815664be647d2bbf918f52fc9afb
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-17 15:34:25 +0200

    Added JS

---
+++ Palindroma/js/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('button');
+
+//Creo un array dove l'utente inserirà la parola
+let parola = [];
+
+//Creo una variabile dove si inserisce la parola
+let item = '';
+
+//Creo un ciclo dove verrà stampato l'ordine inverso della parola
+for(let i = parola.length -1; i >= 0; i--){
+    console.log(parola[i]);
+}
+
+//Creo una funzione che permetterà all'utente di vedere la parola al contrario
+btn.addEventListener('click',{
+
+});
+
+function 
